dbe43d241614bcb2421719fcbc61abd42b7ac806
Oussama Moutafatin <oussama@moutafatin.com>
2025-07-20 18:45:45 +0100

    impl add new task

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use task::Task;
+use task::{Task, TaskManager};
 
 mod task;
 fn main() {
+    let mut app = TaskManager::init();
+
+    app.add_task("Test task");
+
     println!("Task");
 }
--- src/task.rs
+++ src/task.rs
@@ -16,3 +16,23 @@ pub struct TaskManager {
     tasks: HashMap<u32, Task>,
     next_id: u32,
 }
+
+impl TaskManager {
+    pub fn init() -> Self {
+        Self {
+            tasks: HashMap::new(),
+            next_id: 1,
+        }
+    }
+
+    pub fn add_task(&mut self, title: &str) {
+        self.tasks.insert(
+            self.next_id,
+            Task {
+                id: self.next_id,
+                title: title.into(),
+                status: Status::Todo,
+            },
+        );
+    }
+}
